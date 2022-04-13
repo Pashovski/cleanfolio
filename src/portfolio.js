91dd0c1f82022bcf9520069bf272c09607802911
Pashovski <pashdoctor@gmail.com>
2022-04-13 18:20:08 -0400

    first commit

--- src/portfolio.js
+++ src/portfolio.js
@@ -6,14 +6,14 @@ const header = {
 
 const about = {
   // all the properties are optional - can be left empty or deleted
-  name: 'John Smith',
+  name: 'Paul Davidson',
   role: 'Front End Engineer',
   description:
     'Adipisicing sit fugit ullam unde aliquid sequi Facilis soluta facilis perspiciatis corporis nulla aspernatur. Autem eligendi rerum delectus modi quisquam? Illo ut quasi nemo ipsa cumque perspiciatis! Maiores minima consectetur.',
   resume: 'https://example.com',
   social: {
-    linkedin: 'https://linkedin.com',
-    github: 'https://github.com',
+    linkedin: 'https://www.linkedin.com/in/pauledavidson',
+    github: 'https://github.com/Pashovski',
   },
 }
 
@@ -21,28 +21,26 @@ const projects = [
   // projects can be added an removed
   // if there are no projects, Projects section won't show up
   {
-    name: 'Project 1',
-    description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
+    name: 'Support Desk',
+    description: 'MERN app that allows users to create edit support tickets',
+    stack: ['MongoDB', 'Express.js', 'React', 'Node.js'],
+    sourceCode: 'https://github.com/Pashovski/support-desk',
+    livePreview: 'https://davidsonsupportdesk.herokuapp.com/',
   },
   {
-    name: 'Project 2',
+    name: 'House Marketplace',
     description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
+      'A full-stack app that allows users to list their homes for sale or rent',
+    stack: ['React', 'Firebase'],
+    sourceCode: 'https://github.com/Pashovski/house-marketplace',
+    livePreview: 'https://house-marketplace-psi-amber.vercel.app/',
   },
   {
-    name: 'Project 3',
-    description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
+    name: 'Github Finder',
+    description: 'A React App that allows users to search Github Accounts',
+    stack: ['Tailwind', 'DaisyUI', 'React'],
     sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
+    livePreview: 'https://github-finder-three-theta.vercel.app/',
   },
 ]
 
@@ -52,20 +50,17 @@ const skills = [
   'HTML',
   'CSS',
   'JavaScript',
-  'TypeScript',
+  'Firebase',
   'React',
   'Redux',
-  'SASS',
+  'Ruby',
   'Material UI',
   'Git',
-  'CI/CD',
-  'Jest',
-  'Enzyme',
 ]
 
 const contact = {
   // email is optional - if left empty Contact section won't show up
-  email: 'johnsmith@mail.com',
+  email: 'pauleugenedavidson@gmail.com',
 }
 
 export { header, about, projects, skills, contact }
